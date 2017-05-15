244b1ba03924077e8cfa803d3440aecf14d86fe5
xiongjiangwei <xiongjiangwei@wilddog.com>
2017-05-15 10:31:58 +0800

    update

--- initLocal.sh
+++ initLocal.sh
@@ -1,8 +1,9 @@
 hexo generate
-suod npm install -g hexo-cli
+sudo npm install -g hexo-cli
 npm install
 git clone https://github.com/iissnan/hexo-theme-next themes/next
 hexo clean
+rm -f source/_posts/hello-world.md
 cp next/_config.yml themes/next
 cp next/avatar.jpg  themes/next/source/images/
 hexo g
